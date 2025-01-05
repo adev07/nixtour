4078e8da904173255343972a6c9b474a8f0079e2
adev07 <adityaanand1245@gmail.com>
2025-01-05 15:35:11 +0530

    Added react helmet

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import React, { Suspense } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 import GroupBooking from '../pages/group-booking';
 import AboutUs from '../pages/about-us';
 import PrivacyPolicy from '../pages/privacy-policy';
@@ -8,18 +9,77 @@ import TermsConditions from '../pages/terms-conditions';
 
 const Home = React.lazy(() => import('../pages/home'));
 
-
 const Router: React.FC = () => {
   return (
     <BrowserRouter>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/group-booking" element={<GroupBooking />} />
-          <Route path="/about-us" element={<AboutUs />} />
-          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-          <Route path="/contact-us" element={<ContactUs />} />
-          <Route path="/user-agreement" element={<TermsConditions />} />
+          <Route
+            path="/"
+            element={
+              <>
+                <Helmet>
+                  <title>Nixtour - Home</title>
+                </Helmet>
+                <Home />
+              </>
+            }
+          />
+          <Route
+            path="/group-booking"
+            element={
+              <>
+                <Helmet>
+                  <title>Nixtour - Group Booking</title>
+                </Helmet>
+                <GroupBooking />
+              </>
+            }
+          />
+          <Route
+            path="/about-us"
+            element={
+              <>
+                <Helmet>
+                  <title>Nixtour - About Us</title>
+                </Helmet>
+                <AboutUs />
+              </>
+            }
+          />
+          <Route
+            path="/privacy-policy"
+            element={
+              <>
+                <Helmet>
+                  <title>Nixtour - Privacy Policy</title>
+                </Helmet>
+                <PrivacyPolicy />
+              </>
+            }
+          />
+          <Route
+            path="/contact-us"
+            element={
+              <>
+                <Helmet>
+                  <title>Nixtour - Contact Us</title>
+                </Helmet>
+                <ContactUs />
+              </>
+            }
+          />
+          <Route
+            path="/user-agreement"
+            element={
+              <>
+                <Helmet>
+                  <title>Nixtour - Terms & Conditions</title>
+                </Helmet>
+                <TermsConditions />
+              </>
+            }
+          />
         </Routes>
       </Suspense>
     </BrowserRouter>
